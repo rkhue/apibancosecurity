ed118fd3c6682751ba1ce4b94c9ab9f706397445
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-03 11:14:33 -0300

    (feat) annotate AccountRepository and TransactionRepository with @Repository

--- src/main/java/com/apibancosecurity/repository/TransactionRepository.java
+++ src/main/java/com/apibancosecurity/repository/TransactionRepository.java
@@ -2,6 +2,9 @@ package com.apibancosecurity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.apibancosecurity.model.Transaction;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+
 }
