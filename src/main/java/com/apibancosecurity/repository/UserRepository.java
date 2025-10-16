c8139852731e7ed28c928cf2e23e7ad54a8dffc5
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-16 11:41:49 -0300

    (feat) Update password encoder to NoOpPasswordEncoder and add findByUsername method in UserRepository

--- src/main/java/com/apibancosecurity/repository/UserRepository.java
+++ src/main/java/com/apibancosecurity/repository/UserRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
     List<User> findAllByRolesContainsIgnoreCase(String role);
 }
--- src/main/java/com/apibancosecurity/security/SecurityConfig.java
+++ src/main/java/com/apibancosecurity/security/SecurityConfig.java
@@ -5,11 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,24 +17,24 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-//        return NoOpPasswordEncoder.getInstance();
+//        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder encoder) {
-
-        UserDetails admin = User.withUsername("admin")
-                .password(encoder.encode("adminpass"))
-                .roles(UserRole.ADMIN)
-                .build();
-        UserDetails user = User.withUsername("user")
-                .password(encoder.encode("userpass"))
-                .roles(UserRole.USER)
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
-    }
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder encoder) {
+//
+//        UserDetails admin = User.withUsername("admin")
+//                .password(encoder.encode("adminpass"))
+//                .roles(UserRole.ADMIN)
+//                .build();
+//        UserDetails user = User.withUsername("user")
+//                .password(encoder.encode("userpass"))
+//                .roles(UserRole.USER)
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
